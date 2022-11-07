0b4e08854798ddd25649fea7036b3da657028d4b
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-11-07 04:56:41 +0100

    tkinter project tutorial setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="TKInter",
+    version="0.0.1",
+    author="kulkirti Chakma",
+    author_email="kirticse.chakma869@gmail.com",
+    package=find_packages(),
+    install_requires= ["numpy","matplotlib"],
+)
